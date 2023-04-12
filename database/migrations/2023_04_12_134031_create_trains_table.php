0bd733df994b87550ecea1e3a0e3438eeee2a1c7
Jero1304 <calogeroleto13@gmail.com>
2023-04-12 16:50:54 +0200

    ADD migration

---
+++ database/migrations/2023_04_12_134031_create_trains_table.php
@@ -0,0 +1,51 @@
+<?php
+/*
+Ogni treno dovrà avere:
+
+Azienda
+Stazione di partenza
+Stazione di arrivo
+Orario di partenza
+Orario di arrivo
+Codice Treno
+Numero Carrozze
+In orario
+Cancellato
+*/
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency', 20);
+            $table->string('departure_station',60);
+            $table->string('arrival_station',60);
+            // $table->dateTime('departure_time');
+            // $table->dateTime('arrival_time');
+            $table->string('train_code',6);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
